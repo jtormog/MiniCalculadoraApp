f5e9c38ce999984cbac46bbce0396c7daa03225d
Josep Tormo García <joseptormogarcia@gmail.com>
2025-02-19 19:16:11 +0100

    modified:   angular.json 	modified:   package-lock.json 	modified:   package.json 	modified:   src/app/app.component.html 	modified:   src/app/app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'MiniCalculadoraApp';
+  title = 'Mini Calculadora';
+  numero1 : number = 0;
+  numero2 : number = 0;
+  resultado : number = 0;
+
+  sumar() : void{
+    this.resultado = +this.numero1 + +this.numero2;
+  }
+  restar() : void{
+    this.resultado = this.numero1 - this.numero2;
+  }
 }
